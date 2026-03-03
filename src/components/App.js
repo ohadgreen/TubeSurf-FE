11bc0415d8a8e46e42add14e106a3b2b89df306c
ogreen <ohad.green@novidea.com>
2026-03-03 12:14:39 +0200

    remove redundant files

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "./Header";
 import TubeSurfMain from "./TubeSurfMain";
 import "./App.css";
 
 const App = () => {
     return (
         <div className="app-container">
-            {/* <Header /> */}
             <div className="app-content">
                 <TubeSurfMain />
             </div>
